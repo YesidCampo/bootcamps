c6ee8913bcaeead8a8a515da627ea94c34e44ac7
YesidCampo <yesidcampogonzalez@gmail.com>
2024-09-23 08:45:00 -0500

    first commit

---
+++ src/test/java/com/bootcamps/BootcampsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
